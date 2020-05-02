d97c410915e419c2f99e0b8ecfe89e2c8c25cee6
Makino Fumiya <flipflop.macky.github@gmail.com>
2020-05-02 21:48:28 +0900

    審判のような処理をまとめるRefereeクラスのファイルをまず作成

---
+++ Reversi/Referee.swift
@@ -0,0 +1,13 @@
+//
+//  Referee.swift
+//  Reversi
+//
+//  Created by 槇野郁弥 on 2020/05/02.
+//  Copyright © 2020 Yuta Koshizawa. All rights reserved.
+//
+
+import UIKit
+
+class Referee: NSObject {
+
+}
